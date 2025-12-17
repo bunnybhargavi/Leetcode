070dd082387745a8828cea5d4ef2937c45028530
A Bhargavi <147232054+bunnybhargavi@users.noreply.github.com>
2025-12-17 17:47:19 +0530

    Time: 47 ms (44.91%), Space: 52.6 MB (98.25%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let i =n;
+    return function() {
+        return i++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
