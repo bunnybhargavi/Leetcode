9dbbe58a1e2d42e90d3d8d69eb514de4521ed847
A Bhargavi <147232054+bunnybhargavi@users.noreply.github.com>
2025-12-21 18:52:39 +0530

    Time: 4 ms (99.08%), Space: 43.5 MB (79.08%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -3,19 +3,14 @@ class Solution {
         int n1 = s.length();
         int n2 = t.length();
         if(n1 != n2) return false;
-        Map<Character,Character> mp1 = new HashMap<>();
-        Map<Character,Character> mp2 = new HashMap<>();
+        int[] s1 = new int[256];
+        int[] t1 = new int[256];
         for(int i=0; i<n1;i++){
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-            if(mp1.containsKey(c1)){
-                if(mp1.get(c1) != c2) return false;
-            }
-            mp1.put(c1,c2);
-            if(mp2.containsKey(c2)){
-                if(mp2.get(c2) != c1) return false;
-            }
-            mp2.put(c2,c1);
+            if(s1[c1] != t1[c2]) return false;
+            s1[c1] = i+1;
+            t1[c2] = i+1;
         }
         return true;
     }
