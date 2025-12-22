240707e07227404de5b126e7106f08169e5236cb
A Bhargavi <147232054+bunnybhargavi@users.noreply.github.com>
2025-12-22 19:12:20 +0530

    Time: 57 ms (5.37%), Space: 52.6 MB (98.31%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let n = arr.length;
+    let res = [];
+    for(let i=0; i<n;i++){
+        res.push(fn(arr[i],i));
+    }
+    return res;
+};
