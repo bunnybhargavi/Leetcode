e7b59f31b52f165aca06767b9ddc4b13ca48c23b
A Bhargavi <147232054+bunnybhargavi@users.noreply.github.com>
2025-12-25 21:13:32 +0530

    Time: 156 ms (25.4%), Space: 109.3 MB (19.84%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,36 +1,26 @@
-class Data {
-    String val;
-    int time;
-    Data(String val, int time) {
-        this.val = val;
-        this.time = time;
-    }
-}
 class TimeMap {
-
-    Map<String, List<Data>> map;
+    Map<String,TreeMap<Integer,String>> map;
     public TimeMap() {
-        map = new HashMap<String, List<Data>>();
+        map = new HashMap<>();
     }
-
+    
     public void set(String key, String value, int timestamp) {
-        map.computeIfAbsent(key, k -> new ArrayList<Data>()).add(new Data(value, timestamp));
+        map.putIfAbsent(key, new TreeMap<>());
+        map.get(key).put(timestamp, value);
     }
-
+    
     public String get(String key, int timestamp) {
-        if (!map.containsKey(key)) return "";
-        return binarySearch(map.get(key), timestamp);
-    }
-
-    private String binarySearch(List<Data> list, int time) {
-        int left = 0, right = list.size() - 1;
-        while (left < right) {
-            int mid = (left + right + 1) >>> 1;
-            if (time < list.get(mid).time)
-                right = mid - 1;
-            else
-                left = mid;
-        }
-        return list.get(left).time <= time ? list.get(left).val : "";  // left == right
+        if(!map.containsKey(key)) return "";
+        TreeMap<Integer, String> t = map.get(key);
+        Integer tm = t.floorKey(timestamp);
+        if(tm == null) return "";
+        return t.get(tm);
     }
 }
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
