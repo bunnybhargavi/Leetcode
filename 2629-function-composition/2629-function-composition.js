74a4dcaabb7aaf5d7aa279eba99a6ebde5cd51f2
A Bhargavi <147232054+bunnybhargavi@users.noreply.github.com>
2025-12-25 12:52:19 +0530

    Time: 53 ms (78.77%), Space: 55.2 MB (89.92%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -9,10 +9,11 @@ var compose = function(functions) {
     //     }
     // }
     return function(x){
+        let res =x;
         for(let i = functions.length-1; i>= 0;i--){
-            x = functions[i](x);
+            res = functions[i](res);
         }
-        return x;
+        return res;
     }
 };
 
