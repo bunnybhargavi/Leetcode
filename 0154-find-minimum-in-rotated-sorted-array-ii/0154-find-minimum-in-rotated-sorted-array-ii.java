129ed76b9e4957c87916209b96e775bea82582ec
A Bhargavi <147232054+bunnybhargavi@users.noreply.github.com>
2026-01-01 21:34:31 +0530

    Time: 0 ms (100%), Space: 44.9 MB (34.19%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.java
@@ -5,12 +5,12 @@ class Solution {
         int r = n-1;
         while(l < r){
             int mid = l+(r-l)/2;
-            if(nums[mid] == nums[r]){
-                r--;
-            }else if(nums[mid] < nums[r]){
+            if(nums[mid] < nums[r]){
                 r = mid;
-            }else{
+            }else if(nums[mid] > nums[r]){
                 l = mid+1;
+            }else{
+                r--;
             }
         }
         return nums[l];
